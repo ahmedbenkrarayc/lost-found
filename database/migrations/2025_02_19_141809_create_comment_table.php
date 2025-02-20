3d9acdfbbbf3d1dee1fab11884df2a78c77ecdd3
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-02-20 01:42:52 +0100

    created migrations

---
+++ database/migrations/2025_02_19_141809_create_comment_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comment', function (Blueprint $table) {
+            $table->id();
+            $table->string('comment');
+            $table->unsignedBigInteger('annonce_id');
+            $table->unsignedBigInteger('membre_id');
+            $table->foreign('annonce_id')->references('id')->on('annonce')->onDelete('cascade');
+            $table->foreign('membre_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comment');
+    }
+};
