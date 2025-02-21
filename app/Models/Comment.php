583453b7b9c4e56b1c36dc3f6f5c37d3f5849f78
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-02-21 18:25:21 +0100

    created comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $table = 'comment';
+    protected $fillable = [
+        'comment',
+        'annonce_id',
+        'membre_id'
+    ];
 }
