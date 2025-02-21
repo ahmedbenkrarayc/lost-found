1829e02241073a05789399d1ec3abbb1950cf5d6
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-02-21 22:49:28 +0100

    added filter feature to home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,21 +9,13 @@
 <body class="px-20 overflow-x-hidden">
     <main>
         <header class="text-[#1c1d20] relative">
-            <nav class="flex justify-between py-11 font-Quicksand mb-16">
-                <h1 class="sm:text-[19px] md:text-[18px] lg:text-[20px] font-[600]">Lost&Found</h1>
-                <ul class="flex gap-x-6 sm:text-[19px] md:text-[16px] lg:text-[16px] font-[600]">
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Statistics</a></li>
-                    <li><a href="">Login</a></li>
-                    <li><a href="">Register</a></li>
-                </ul>
-            </nav>
+            @include('layouts.nav')
     
             <h1 class="sm:w-[90%] md:w-[80%] sm:text-[50px] md:text-[70px] lg:text-[80px] xl:text-[100px] font-sans font-semibold">Connecting people to reunite lost belongings.</h1>
             <img class="sm:hidden md:block w-[70rem] z-[-1] top-[300px] right-[-40%] bottom-0 left-auto absolute opacity-[.83]" src="https://ahmedbenkrara.netlify.app/assets/highlight-ElSyRTm5.png" alt="highlight">
         </header>
         <div class="mt-11">
-            <input placeholder="Search ..." class="w-1/2 bg-white text-[#1c1d20] border border-[#c0c0c0] mb-10 h-fit rounded-full sm:px-8 sm:py-[18px] md:px-9 md:py-[20px] lg:px-11 lg:py-[25px] font-Quicksand sm:text-[15px] md:text-[16px] font-medium"/>
+            <input id="keyword" type="text" placeholder="Search ..." class="w-1/2 bg-white text-[#1c1d20] border border-[#c0c0c0] mb-10 h-fit rounded-full sm:px-8 sm:py-[18px] md:px-9 md:py-[20px] lg:px-11 lg:py-[25px] font-Quicksand sm:text-[15px] md:text-[16px] font-medium"/>
             <div id="categories" class="flex gap-x-4 gap-y-6 flex-wrap">
                 <div class="category selectedcat">All</div>
             </div>
