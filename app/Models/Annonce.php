95f5714f6ff21c358919d94d0d62b92f12725a0f
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-02-21 18:24:34 +0100

    created annonce model

--- app/Models/Annonce.php
+++ app/Models/Annonce.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Annonce extends Model
 {
-    //
+    protected $table = 'annonce';
+    protected $fillable = [
+        'title',
+        'description',
+        'cover',
+        'date',
+        'place',
+        'phone',
+        'type',
+        'category_id',
+        'users_id'
+    ];
+
 }
